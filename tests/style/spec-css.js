86b9f3333375ba45045e1951098c9067dd9b6f70
Conrad Zimmerman <me@conradz.com>
2013-05-22 10:27:53 -0400

    Change style and simplify tests

--- tests/style/spec-css.js
+++ tests/style/spec-css.js
@@ -1,35 +1,31 @@
-define( [ "cane/style/css" ], function ( css ) {
+define(["cane/style/css"], function(css) {
 
-	describe( "style/css()", function () {
+    describe("style/css()", function() {
 
-		it( "it should add one style in key value format", function () {
+        it("should add one style in key value format", function() {
+            var node = document.createElement("div");
 
-			var node = document.createElement( "div" );
+            css(node, "overflow", "hidden");
 
-			css( node, "overflow", "hidden" );
+            expect(node.style.overflow).toBe("hidden");
+        });
 
-			expect( node.style.overflow ).toBe( "hidden" );
+        it("should add multiple style settings passed as object", function() {
+            var node = document.createElement("div");
 
-		} );
+            css(node, {
+                "overflow": "hidden",
+                "height": "30px",
+                "width": "20px",
+                "lineHeight": "20px"
+            });
 
-		it ( "it should add multiple style settings passed as object", function() {
+            expect(node.style.overflow).toBe("hidden");
+            expect(node.style.height).toBe("30px");
+            expect(node.style.width).toBe("20px");
+            expect(node.style.lineHeight).toBe("20px");
+        });
 
-			var node = document.createElement( "div" );
+    });
 
-			css( node, {
-				"overflow": "hidden",
-				"height": "30px",
-				"width": "20px",
-				"lineHeight": "20px"
-			} );
-
-			expect( node.style.overflow ).toBe( "hidden" );
-			expect( node.style.height ).toBe( "30px" );
-			expect( node.style.width ).toBe( "20px" );
-			expect( node.style.lineHeight ).toBe( "20px" );
-
-		} );
-
-	} );
-
-} );
+});
