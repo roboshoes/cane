36985a70b71f38136be82590e01f8c07659a8f6a
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-11-06 16:18:42 -0800

    update local test server

--- server/main.js
+++ server/main.js
@@ -1,20 +1,52 @@
-var express = require("express");
-var clone = require("mout/lang/clone");
+var express = require( "express" );
+var clone = require( "mout/lang/clone" );
+
 var app = express();
 
-app.get("/jsonp/static", function(request, result) {
-	result.jsonp({
+app.get( "/jsonp/static", function( request, response ) {
+	response.jsonp( {
 		foo: "bar"
-	});
-});
+	} );
+} );
 
-app.get("/jsonp/mirror", function(request, result) {
-	var params = clone(request.query);
+app.get( "/jsonp/mirror", function( request, response ) {
+	var params = clone( request.query );
 	delete params.callback;
 
-	result.jsonp(params);
+	response.jsonp( params );
 });
 
-app.listen(9000);
+app.get( "/resources/:name", function( request, response ) {
+
+    var options = {
+        root: __dirname + "/../tests/resources",
+        dotfiles: "deny",
+        headers: {
+            "x-timestamp": Date.now(),
+            "x-sent": true
+        }
+    };
+
+    var fileName = request.params.name;
+
+    response.sendFile( fileName, options, function( error ) {
+
+        if ( error ) {
+            response.status( error.status ).end();
+        }
+
+    } );
+} );
+
+app.get( "/", function( request, response ) {
+    response.send( "<!DOCTYPE html><html><body><script type='text/javascript' src='bundle.js'></script></body></html>" );
+} );
+
+app.get( "/bundle.js", function( request, response ) {
+    response.sendFile( __dirname + "/bundle.js" );
+} );
+
+app.listen( 9000 );
 
 module.exports = app;
+
